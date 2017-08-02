b9c965725035749608034fb6d58c469db07da3ed
williamtwobit <william@twobit.cc>
2017-08-02 12:26:09 -0400

    implemented exercise 4

--- app.js
+++ app.js
@@ -175,10 +175,68 @@ function bfs(tree, values=[]){
 }
 
 // Exercise 1
+// just use a linear search
 
-// use a linear search
 
 // Exercise 2
 // let str = "89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5 76 62";
 // const arr = str.split(' ').map(n => Number(n)).sort((a, b) => a - b);
 // console.log(binaryArrSearch(arr, 45));
+
+
+// Exercise 3
+// Skipped for now
+
+
+//Exercise 4
+const ex4Tree = new BinarySearchTree;
+function populateTree(valArr, tree){
+  for(let i=0; i<valArr.length; i++){
+    tree.insert(valArr[i], valArr[i]);
+  }
+  // console.log('Tree: ', heightTree);
+}
+let str = '25 15 50 10 24 35 70 4 12 18 31 44 66 90 22';
+const arr = str.split(' ').map(n => Number(n));
+populateTree(arr, ex4Tree);
+
+function dfsPreOrder(tree, val) {
+  console.log(tree.key);
+  if (tree.left) {
+    dfsPreOrder(tree.left, val);
+  }
+  if (tree.right) {
+    dfsPreOrder(tree.right, val);
+  }
+}
+
+function dfsInOrder(tree, val) {
+  if (tree.left) {
+    dfsInOrder(tree.left, val);
+  }
+  console.log(tree.key);
+  if (tree.right) {
+    dfsInOrder(tree.right, val);
+  }
+}
+
+function dfsPostOrder(tree, val) {
+  if (tree.left) {
+    dfsPostOrder(tree.left, val);
+  }
+
+  if (tree.right) {
+    dfsPostOrder(tree.right, val);
+  }
+  console.log(tree.key);
+}
+console.log('<<<<<<<<< PRE ORDER >>>>>>>>>>');
+dfsPreOrder(ex4Tree);
+console.log();
+console.log();
+console.log('<<<<<<<<< IN ORDER >>>>>>>>>>');
+dfsInOrder(ex4Tree);
+console.log();
+console.log();
+console.log('<<<<<<<<< POST ORDER >>>>>>>>>>');
+dfsPostOrder(ex4Tree);
